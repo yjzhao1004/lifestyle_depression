40455a4f0448088f778755f36c7d57e27523107b
yjzhao1004 <123579364+yjzhao1004@users.noreply.github.com>
2023-08-03 10:12:45 +0800

    cal_sem.R

--- cal_sem.R
+++ cal_sem.R
@@ -1,37 +1,40 @@
-setwd("/Users/yujiezhao/Desktop/SEM_1108/")
+setwd("/Users/yujiezhao/Desktop/Lifestyle_Depression/SEM_20230406")
 rm(list = ls())
 library(lavaan)
-Sem_data0<-read.csv("regre_mergedata_notiv_1129.csv")
+Sem_data0<-read.csv("regre_mergedata_20230408_fullbrain.csv")
 
-##CFA
+
+###CFA
 cfamodel<-'    depression =~ 1*phq1_ins0 + phq2_ins0 + phq3_ins0 + phq4_ins0 
                lifestyle  =~ 1*lifescore
-               vol        =~ 1*Left_Pallidum+rh_precentral_volume+lh_precentral_volume+Left_Thalamus_Proper+lh_superiorfrontal_volume+Right_Pallidum+Right_Hippocampus+rh_insula_volume+lh_parahippocampal_volume+Right_Thalamus_Proper+rh_parsorbitalis_volume+Left_Hippocampus+lh_medialorbitofrontal_volume+rh_medialorbitofrontal_volume+lh_lateralorbitofrontal_volume+rh_parahippocampal_volume+lh_parsorbitalis_volume+rh_lateralorbitofrontal_volume+lh_lingual_volume+lh_paracentral_volume
+               vol        =~ 1*Left_Pallidum+lh_precentral_volume+Right_Pallidum+rh_precentral_volume+lh_superiorfrontal_volume+Left_Thalamus_Proper+lh_middletemporal_volume+lh_parahippocampal_volume+rh_insula_volume+Left_Amygdala+lh_posteriorcingulate_volume+Right_Thalamus_Proper+lh_insula_volume+rh_middletemporal_volume+rh_parahippocampal_volume+lh_medialorbitofrontal_volume+lh_fusiform_volume+lh_lateraloccipital_volume+rh_medialorbitofrontal_volume+rh_inferiorparietal_volume                                           
                immu       =~ 1*Glucose + C.reactive.protein + Triglycerides +Glycated.haemoglobin..HbA1c. 
-               PRSL       =~ 1*Pt_0.05
+               PRSL       =~ 1*PRS
 
                depression~~lifestyle + vol + immu +PRSL
                vol ~~ vol + immu + PRSL
                immu~~PRSL
 '
-
 fit <- cfa(cfamodel, data = Sem_data0,std.lv = TRUE)
 summary(fit,fit.measures=T)
 
+
+
 ###SEM 
+'
 sem_model_0 <- '
 # measurement model
-  lifestyle  =~ lifescore
-  vol        =~ Left_Pallidum+rh_precentral_volume+lh_precentral_volume+Left_Thalamus_Proper+lh_superiorfrontal_volume+Right_Pallidum+Right_Hippocampus+rh_insula_volume+lh_parahippocampal_volume+Right_Thalamus_Proper+rh_parsorbitalis_volume+Left_Hippocampus+lh_medialorbitofrontal_volume+rh_medialorbitofrontal_volume+lh_lateralorbitofrontal_volume+rh_parahippocampal_volume+lh_parsorbitalis_volume+rh_lateralorbitofrontal_volume+lh_lingual_volume+lh_paracentral_volume
+  lifestyle  =~ lifestylescore
+  vol        =~ Left_Pallidum+lh_precentral_volume+Right_Pallidum+rh_precentral_volume+lh_superiorfrontal_volume+Left_Thalamus_Proper+lh_middletemporal_volume+lh_parahippocampal_volume+rh_insula_volume+Left_Amygdala+lh_posteriorcingulate_volume+Right_Thalamus_Proper+lh_insula_volume+rh_middletemporal_volume+rh_parahippocampal_volume+lh_medialorbitofrontal_volume+lh_fusiform_volume+lh_lateraloccipital_volume+rh_medialorbitofrontal_volume+rh_inferiorparietal_volume                                           
   depression =~ phq1_ins0 + phq2_ins0 + phq3_ins0 + phq4_ins0 
-  immu       =~ Glucose + C.reactive.protein + Triglycerides +Glycated.haemoglobin..HbA1c. 
-  PRSL       =~ Pt_0.05
+  immu       =~ C.reactive.protein + Triglycerides + Glycated.haemoglobin..HbA1c.  +  Glucose
+  PRSL       =~ PRS
   
 # structural model
-  depression ~ vol + lifestyle  +PRSL +immu 
-  lifestyle  ~ PRSL  
-  immu       ~ lifestyle + PRSL 
-  vol        ~ PRSL + lifestyle+immu
+  depression ~ PRSL+ immu +vol +lifestyle
+  lifestyle  ~ PRSL 
+  immu       ~ PRSL + lifestyle
+  vol        ~ PRSL+lifestyle+immu
 '
 
 fit_model <- sem(sem_model_0, data = Sem_data0, std.lv = TRUE)
